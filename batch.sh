181d86a2be1ab3524b40a86430973ae625b96df4
Ibnu Taimiyyah <ibnu2651@gmail.com>
2026-04-01 18:50:42 +0800

    Change batch file

--- batch.sh
+++ batch.sh
@@ -8,4 +8,4 @@
 #SBATCH -p gpu
 
 srun python script.py --dataset BasicMotions --task_type classification --batch 32 --epochs 100
-srun python prune_finetune_test.py --checkpoint checkpoints/BasicMotions_best.pt --dataset BasicMotions --batch 32 --new_nhid_tar 64 --new_nhid_task 64 --new_num_layers 1 --epochs 10 --output checkpoints/BasicMotions_pruned_finetuned.pt
+srun python prune_finetune_test.py --checkpoint checkpoints/BasicMotions_best.pt --dataset BasicMotions --batch 32 --new_nhid_tar 64 --new_nhid_task 64 --epochs 10 --output checkpoints/BasicMotions_pruned_finetuned.pt
