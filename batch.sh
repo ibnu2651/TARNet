3c3057ce84bc9c8b1730d46a79111b116796e8a2
Ibnu Taimiyyah <ibnu2651@gmail.com>
2026-04-01 18:53:09 +0800

    Change batch file

--- batch.sh
+++ batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=tarnet
-#SBATCH --output=tarnet_train_64_2.slurmlog
-#SBATCH --error=tarnet_train_64_2.slurmlog
+#SBATCH --output=tarnet_train_128_1.slurmlog
+#SBATCH --error=tarnet_train_128_1.slurmlog
 #SBATCH --time=02:00:00
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 #SBATCH -p gpu
 
 srun python script.py --dataset BasicMotions --task_type classification --batch 32 --epochs 100
-srun python prune_finetune_test.py --checkpoint checkpoints/BasicMotions_best.pt --dataset BasicMotions --batch 32 --new_nhid_tar 64 --new_nhid_task 64 --epochs 10 --output checkpoints/BasicMotions_pruned_finetuned.pt
+srun python prune_finetune_test.py --checkpoint checkpoints/BasicMotions_best.pt --dataset BasicMotions --batch 32 --new_num_layers 1 --epochs 10 --output checkpoints/BasicMotions_pruned_finetuned.pt
