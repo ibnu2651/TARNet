c0834ddf215889c43dbdfc3c6fb1d46e48b8eddb
Ibnu Taimiyyah <ibnu2651@gmail.com>
2026-04-01 17:26:03 +0800

    Add batch script

---
+++ batch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=tarnet
+#SBATCH --output=tarnet_train.slurmlog
+#SBATCH --error=tarnet_train.slurmlog
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu
+
+srun python script.py --dataset AF --task_type classification
